103f1d489023327a2c5929a51334c8b1473dd8b4
LukasBrits <martinbrits.mb@gmail.com>
2021-10-30 07:24:05 -0700

    First Get Functions
    Added getUser functions by ID and student numbers

--- src/com.app.data.access.objects/userDao.js
+++ src/com.app.data.access.objects/userDao.js
@@ -1,42 +1,49 @@
 class UserDAO {
+  constructor(username, password, session, channel, jsonData) {
+    this.username = username;
+    this.password = password;
+    this.session = session;
+    this.channel = channel;
+    this.jsonData = jsonData;
+  }
 
-    constructor(username, password, session, channel, jsonData) {
-        this.username = username;
-        this.password = password;
-        this.session = session;
-        this.channel = channel;
-        this.jsonData = jsonData;
-    }
+  CheckUserSQL() {
+    let params = `'${this.username}','${this.password}','${this.session}',
+        '${this.channel}','${this.jsonData}'`;
+    let sql = `CALL fn_profile_login(${params})`;
+    return sql;
+  }
 
-    CheckUserSQL() {
-        let params = (`'${this.username}','${this.password}','${this.session}',
-        '${this.channel}','${this.jsonData}'`);
-        let sql = `CALL fn_profile_login(${params})`
-        return sql;
-    }
+  static GetUserbyIDSQL(obj) {
+    console.log("Session: " + obj.session);
+    console.log("UserId: " + obj.userId);
+    let params = `'${obj.session}','${obj.userId}'`;
+    let sql = `CALL sp_profile_details_get(${params})`;
+    return sql;
+  }
 
-    static GetUserSQL(obj) {
-        let params = (`'${obj.profileId}','${obj.session}',
-        '${obj.channel}','${obj.jsonData}'`);
-        let sql = `CALL fn_profile_details_get(${params})`
-        return sql;
-    }
+  static GetUserbyStudentNumberSQL(obj) {
+    console.log("Session: " + obj.session);
+    console.log("StudentNumber: " + obj.studentNumber);
+    let params = `'${obj.session}','${obj.studentNumber}'`;
+    let sql = `CALL sp_profile_details_by_student_number_get(${params})`;
+    return sql;
+  }
 
-    static UpdateUserByIdSQL(obj) {
-        let params = (`'${obj.profileId}','${obj.session}','${obj.firstName}',
+  static UpdateUserByIdSQL(obj) {
+    let params = `'${obj.profileId}','${obj.session}','${obj.firstName}',
         '${obj.lastName}','${obj.email}','${obj.phone}',
-        '${obj.channel}','${obj.jsonData}'`);
-        let sql = `CALL fn_profile_details_update(${params})`
-        return sql;
-    }
+        '${obj.channel}','${obj.jsonData}'`;
+    let sql = `CALL fn_profile_details_update(${params})`;
+    return sql;
+  }
 
-    static UpdateUser2FaSQL(obj) {
-        let params = (`'${obj.profileId}','${obj.session}',
-        '${obj.status}','${obj.channel}','${obj.jsonData}'`);
-        let sql = `CALL fn_profile_tfa_update(${params})`
-        return sql;
-    }
-
+  static UpdateUser2FaSQL(obj) {
+    let params = `'${obj.profileId}','${obj.session}',
+        '${obj.status}','${obj.channel}','${obj.jsonData}'`;
+    let sql = `CALL fn_profile_tfa_update(${params})`;
+    return sql;
+  }
 }
 
 module.exports = UserDAO;
--- src/com.app.modules/user.js
+++ src/com.app.modules/user.js
@@ -1,113 +1,147 @@
-var define = require('node-requirejs-define'),
-    rulecfg = require('../com.app.configuration/ruleConf.json'),
-    validation = require('../com.app.utils/validationHandler'),
-    usrDao = require('../com.app.data.access.objects/userDao'),
-    bseDao = require('../com.app.data.access.objects/baseDao'),
-    dispatch = require('../com.app.service.layer/dispatcher'),
-    logger = require('../com.app.utils/logHandler').Logger;
+var define = require("node-requirejs-define"),
+  rulecfg = require("../com.app.configuration/ruleConf.json"),
+  validation = require("../com.app.utils/validationHandler"),
+  usrDao = require("../com.app.data.access.objects/userDao"),
+  bseDao = require("../com.app.data.access.objects/baseDao"),
+  dispatch = require("../com.app.service.layer/dispatcher"),
+  logger = require("../com.app.utils/logHandler").Logger;
 
-define(function() {
+define(function () {
+  function GetUser(req, res, next) {
+    console.log("req.body.id: " + req.body.id);
+    let gt_user = {
+      session: req.headers["apisessionkey"],
+      userId: req.body.id,
+    };
+    bseDao.Query(usrDao.GetUserbyIDSQL(gt_user), (err, resp) => {
+      if (err) {
+        dispatch.SendDataBaseErrorMessage(res, err);
+      } else {
+        var data = JSON.parse(resp);
 
-    function GetUser(req, res, next) {
-        try {
-            const validationRule = rulecfg.validation.userGet;
-            validation.Validator(req.body, validationRule, {}, (err, status) => {
-                if (!status) {
-                    dispatch.SendBadRequestMessage(res, err);
-                    logger.error('/getUser', JSON.stringify(err));
-                } else {
-                    var jsnReq = { profileId: req.body.profileId, session: req.headers['apisessionkey'], channel: req.body.channel },
-                        jsnDta = JSON.stringify(jsnReq);
+        dispatch.SendGenricMessage(res, data.userData);
+        logger.info("/getUser", JSON.stringify(data));
+      }
+    });
+  }
 
-                    logger.info('/getUser', jsnDta);
+  function GetUserbyStudentNumber(req, res, next) {
+    console.log("StudentNr: " + req.body.studentNr);
+    let gt_user = {
+      session: req.headers["apisessionkey"],
+      studentNumber: req.body.studentNr,
+    };
+    bseDao.Query(usrDao.GetUserbyStudentNumberSQL(gt_user), (err, resp) => {
+      if (err) {
+        dispatch.SendDataBaseErrorMessage(res, err);
+      } else {
+        var data = JSON.parse(resp);
 
-                    let gt_user = { profileId: req.body.profileId, session: req.headers['apisessionkey'], channel: req.body.channel, jsonData: jsnDta };
-                    bseDao.Query(usrDao.GetUserSQL(gt_user), (err, resp) => {
-                        if (err) {
-                            dispatch.SendDataBaseErrorMessage(res, err);
-                        } else {
-                            var data = JSON.parse(resp);
+        dispatch.SendGenricMessage(res, data.userData);
+        logger.info("/getUserbyStudentNumber", JSON.stringify(data));
+      }
+    });
+  }
 
-                            dispatch.SendGenricMessage(res, data);
-                            logger.info('/getUser', JSON.stringify(data));
-                        }
-                    });
-                }
-            });
-        } catch (e) {
-            logger.error('/authentication', e);
-            dispatch.DispatchErrorMessage(res, 'application error in GetUser()..');
+  function UpdateUser(req, res, next) {
+    try {
+      const validationRule = rulecfg.validation.userUpdate;
+      validation.Validator(req.body, validationRule, {}, (err, status) => {
+        if (!status) {
+          dispatch.SendBadRequestMessage(res, err);
+          logger.error("/updateUser", JSON.stringify(err));
+        } else {
+          var jsnReq = {
+              profileId: req.body.profileId,
+              session: req.headers["apisessionkey"],
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              email: req.body.emailAddress,
+              phone: req.body.phoneNumber,
+              channel: req.body.channel,
+            },
+            jsnDta = JSON.stringify(jsnReq);
+
+          logger.info("/updateUser", jsnDta);
+
+          let up_user = {
+            profileId: req.body.profileId,
+            session: req.headers["apisessionkey"],
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.emailAddress,
+            phone: req.body.phoneNumber,
+            channel: req.body.channel,
+            jsonData: jsnDta,
+          };
+          bseDao.Query(usrDao.UpdateUserByIdSQL(up_user), (err, resp) => {
+            if (err) {
+              dispatch.SendDataBaseErrorMessage(res, err);
+            } else {
+              var data = JSON.parse(resp);
+
+              dispatch.SendGenricMessage(res, data);
+              logger.info("/updateUser", JSON.stringify(data));
+            }
+          });
         }
+      });
+    } catch (e) {
+      logger.error("/updateUser", e);
+      dispatch.DispatchErrorMessage(res, "application error in UpdateUser()..");
     }
+  }
 
-    function UpdateUser(req, res, next) {
-        try {
-            const validationRule = rulecfg.validation.userUpdate;
-            validation.Validator(req.body, validationRule, {}, (err, status) => {
-                if (!status) {
-                    dispatch.SendBadRequestMessage(res, err);
-                    logger.error('/updateUser', JSON.stringify(err));
-                } else {
-                    var jsnReq = { profileId: req.body.profileId, session: req.headers['apisessionkey'], firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.emailAddress, phone: req.body.phoneNumber, channel: req.body.channel },
-                        jsnDta = JSON.stringify(jsnReq);
+  function TwoFactorAuth(req, res, next) {
+    try {
+      const validationRule = rulecfg.validation.userTwoFactorAuth;
+      validation.Validator(req.body, validationRule, {}, (err, status) => {
+        if (!status) {
+          dispatch.SendBadRequestMessage(res, err);
+          logger.error("/twoFactorAuth", JSON.stringify(err));
+        } else {
+          var jsnReq = {
+              profileId: req.body.profileId,
+              session: req.headers["apisessionkey"],
+              status: req.body.status,
+              channel: req.body.channel,
+            },
+            jsnDta = JSON.stringify(jsnReq);
 
-                    logger.info('/updateUser', jsnDta);
+          logger.info("/twoFactorAuth", jsnDta);
 
-                    let up_user = { profileId: req.body.profileId, session: req.headers['apisessionkey'], firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.emailAddress, phone: req.body.phoneNumber, channel: req.body.channel, jsonData: jsnDta };
-                    bseDao.Query(usrDao.UpdateUserByIdSQL(up_user), (err, resp) => {
-                        if (err) {
-                            dispatch.SendDataBaseErrorMessage(res, err);
-                        } else {
-                            var data = JSON.parse(resp);
+          let tfa = {
+            profileId: req.body.profileId,
+            session: req.headers["apisessionkey"],
+            status: req.body.status,
+            channel: req.body.channel,
+            jsonData: jsnDta,
+          };
+          bseDao.Query(usrDao.UpdateUser2FaSQL(tfa), (err, resp) => {
+            if (err) {
+              dispatch.SendDataBaseErrorMessage(res, err);
+            } else {
+              var data = JSON.parse(resp);
 
-                            dispatch.SendGenricMessage(res, data);
-                            logger.info('/updateUser', JSON.stringify(data));
-                        }
-                    });
-                }
-            });
-        } catch (e) {
-            logger.error('/updateUser', e);
-            dispatch.DispatchErrorMessage(res, 'application error in UpdateUser()..');
+              dispatch.SendGenricMessage(res, data);
+              logger.info("/twoFactorAuth", JSON.stringify(data));
+            }
+          });
         }
+      });
+    } catch (e) {
+      logger.error("/twoFactorAuth", e);
+      dispatch.DispatchErrorMessage(
+        res,
+        "application error in TwoFactorAuth().."
+      );
     }
+  }
 
-    function TwoFactorAuth(req, res, next) {
-        try {
-            const validationRule = rulecfg.validation.userTwoFactorAuth;
-            validation.Validator(req.body, validationRule, {}, (err, status) => {
-                if (!status) {
-                    dispatch.SendBadRequestMessage(res, err);
-                    logger.error('/twoFactorAuth', JSON.stringify(err));
-                } else {
-                    var jsnReq = { profileId: req.body.profileId, session: req.headers['apisessionkey'], status: req.body.status, channel: req.body.channel },
-                        jsnDta = JSON.stringify(jsnReq);
-
-                    logger.info('/twoFactorAuth', jsnDta);
-
-                    let tfa = { profileId: req.body.profileId, session: req.headers['apisessionkey'], status: req.body.status, channel: req.body.channel, jsonData: jsnDta };
-                    bseDao.Query(usrDao.UpdateUser2FaSQL(tfa), (err, resp) => {
-                        if (err) {
-                            dispatch.SendDataBaseErrorMessage(res, err);
-                        } else {
-                            var data = JSON.parse(resp);
-
-                            dispatch.SendGenricMessage(res, data);
-                            logger.info('/twoFactorAuth', JSON.stringify(data));
-                        }
-                    });
-                }
-            });
-        } catch (e) {
-            logger.error('/twoFactorAuth', e);
-            dispatch.DispatchErrorMessage(res, 'application error in TwoFactorAuth()..');
-        }
-    }
-
-    return {
-        GetUser: GetUser,
-        UpdateUser: UpdateUser,
-        TwoFactorAuth: TwoFactorAuth
-    }
-
+  return {
+    GetUser: GetUser,
+    GetUserbyStudentNumber: GetUserbyStudentNumber,
+    UpdateUser: UpdateUser,
+    TwoFactorAuth: TwoFactorAuth,
+  };
 });
